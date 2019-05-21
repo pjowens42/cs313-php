1c24b1080bf69b5f800cee086b9d0edc871e535e
pjowens42 <43393006+pjowens42@users.noreply.github.com>
2019-05-21 00:10:05 -0600

    Add files via upload

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,1 +1,17 @@
+CREATE TABLE users_table (  
+    users_id SERIAL PRIMARY KEY,
+    users_name VARCHAR(32),
+    users_email VARCHAR(32)
+)
 
+CREATE TABLE reviews_table (
+    reviews_id SERIAL PRIMARY KEY,
+    reviews_data VARCHAR(200),
+    reviews_name VARCHAR(32) REFERENCES restaurant_table(restaurant_id)
+)
+
+CREATE TABLE restaurant_table (
+    restaurant_id SERIAL PRIMARY KEY,
+    restaurant_name VARCHAR (32),
+    restaurant_address VARCHAR (100)
+)
