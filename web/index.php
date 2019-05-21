220f40cf9dbe65cbd87c8ec109a4a05e43df2932
pjowens42 <43393006+pjowens42@users.noreply.github.com>
2019-05-21 00:07:51 -0600

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,5 +1,45 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="about.css">
+    <title>Paul Home</title>
+</head>
+<body>
+<div class="jumbotron text-center">
+  <h1>Paul Owens</h1>
+  <p><a href="about.html">adout me</a></p> 
+</div>
 
-  phpinfo();
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Team activities</h3>
+      <ul>
+      <li><a class="btn btn-primary" href="team01\main.html" role="button">Team01</a></li>
+      <li><a class="btn btn-primary" href="team02\team02.php" role="button">Team02</a></li>
+      <li><a class="btn btn-primary" href="team03\team03.php" role="button">Team03</a></li>
+      </ul>
+    </div>
+    <div class="col-sm-4">
+      <h3>Assignments</h3>
+      <ul>
+      <li><a class="btn btn-primary" href="assignment03\shopping-cart.php" role="button">Assignment03</a></li>
+      </ul>
+    </div>
+    <div class="col-sm-4">
+      <h3>Projects</h3>        
+      <ul>
+      <li><a class="btn btn-primary" href="projects01\main.html" role="button">Projects01</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
 
-?>
+</body>
+</html>
